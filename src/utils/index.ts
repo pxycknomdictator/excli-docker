71625c1bdf314e6e9640f1851b90f8c8c99c18b4
Noman <pxycknomdictator@outlook.com>
2026-01-13 03:16:40 +0500

    feat: add fn for append existing file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { writeFile } from "node:fs/promises";
+import { writeFile, appendFile } from "node:fs/promises";
 import { getDockerfile } from "src/docker/builder";
 import { getDockerComposeFile } from "src/docker/compose";
 import type { Config, GenerateFileArgs } from "src/types";
@@ -7,6 +7,11 @@ import { dockerfileLocation, dockerComposeFileLocation } from "src/config";
 export async function generateFile(fileArgs: GenerateFileArgs) {
     const { fileLocation, fileContent } = fileArgs;
     await writeFile(fileLocation, fileContent, "utf-8");
+}
+
+export async function appendExistsFile(fileArgs: GenerateFileArgs) {
+    const { fileLocation, fileContent } = fileArgs;
+    await appendFile(fileLocation, fileContent, "utf-8");
 }
 
 export async function setupDocker(config: Config) {
