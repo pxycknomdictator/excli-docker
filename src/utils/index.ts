55d9732dcf69572ee8c9e64998c1e10f0513823f
Noman <pxycknomdictator@outlook.com>
2026-01-13 02:33:00 +0500

    feat: add setup docker utility fn in codebase

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,27 @@
 import { writeFile } from "node:fs/promises";
-import type { GenerateFileArgs } from "src/types";
+import { getDockerfile } from "src/docker/builder";
+import { getDockerComposeFile } from "src/docker/compose";
+import type { Config, GenerateFileArgs } from "src/types";
+import { dockerfileLocation, dockerComposeFileLocation } from "src/config";
 
 export async function generateFile(fileArgs: GenerateFileArgs) {
     const { fileLocation, fileContent } = fileArgs;
     await writeFile(fileLocation, fileContent, "utf-8");
+}
+
+export async function setupDocker(config: Config) {
+    const dockerContent = getDockerfile(config);
+    const dockerComposeContent = getDockerComposeFile(config);
+
+    const docker: GenerateFileArgs[] = [
+        { fileLocation: dockerfileLocation, fileContent: dockerContent },
+        {
+            fileLocation: dockerComposeFileLocation,
+            fileContent: dockerComposeContent,
+        },
+    ];
+
+    await Promise.all(
+        docker.map(async (whale) => await generateFile({ ...whale })),
+    );
 }
