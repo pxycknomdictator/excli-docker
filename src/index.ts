2f7132897be34ef1ca957d9fb82ea698ed459f50
Noman <pxycknomdictator@outlook.com>
2026-01-14 00:20:32 +0500

    feat: add cli logic in main executable file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello @excli/docker");
+import { setupDocker } from "./utils";
+import { setupEnv } from "./generators";
+import { getUserInputs } from "./cli/prompts";
+
+async function main() {
+    const config = getUserInputs();
+    await Promise.all([setupDocker(config), setupEnv(config)]);
+}
+
+main().catch((error) => {
+    console.error("❌ Error creating project:", error);
+    process.exit(1);
+});
