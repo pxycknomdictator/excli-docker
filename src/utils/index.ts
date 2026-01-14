a30fc5fb3b834f7ba687a8d7b97e4fc8a4dc6407
Noman <pxycknomdictator@outlook.com>
2026-01-15 01:36:16 +0500

    feat: generate docker ignore file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,12 @@ import { writeFile, appendFile } from "node:fs/promises";
 import { getDockerfile } from "src/docker/builder";
 import { getDockerComposeFile } from "src/docker/compose";
 import type { Config, GenerateFileArgs } from "src/types";
-import { dockerfileLocation, dockerComposeFileLocation } from "src/config";
+import {
+    dockerfileLocation,
+    dockerComposeFileLocation,
+    dockerIgnoreFileLocation,
+} from "src/config";
+import { getDockerIgnoreFile } from "src/docker/dockerignore";
 
 export async function generateFile(fileArgs: GenerateFileArgs) {
     const { fileLocation, fileContent } = fileArgs;
@@ -17,12 +22,17 @@ export async function appendExistsFile(fileArgs: GenerateFileArgs) {
 export async function setupDocker(config: Config) {
     const dockerContent = getDockerfile(config);
     const dockerComposeContent = getDockerComposeFile(config);
+    const dockerIgnoreContent = getDockerIgnoreFile();
 
     const docker: GenerateFileArgs[] = [
         { fileLocation: dockerfileLocation, fileContent: dockerContent },
         {
             fileLocation: dockerComposeFileLocation,
             fileContent: dockerComposeContent,
+        },
+        {
+            fileLocation: dockerIgnoreFileLocation,
+            fileContent: dockerIgnoreContent,
         },
     ];
 
