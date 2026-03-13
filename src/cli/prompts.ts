83b3ebb2f9802f493f0e6d3c954e7dd20e29138f
Noman <pxycknomdictator@outlook.com>
2026-03-13 23:50:10 +0500

    refactor: all cli prompt to user interactive

--- src/cli/prompts.ts
+++ src/cli/prompts.ts
@@ -1,52 +1,54 @@
 import { isCancel, select, confirm } from "@clack/prompts";
 import { terminate } from "src/utils";
-import type { Config } from "src/types";
+import type { Config, INTERACTIVE_PROMPTS } from "src/types";
+import { databases, languages, pkg_managers } from "src/config";
 
 async function promptLanguage(): Promise<Config["language"]> {
-    const language = (await select({
+    const language = await select({
         message: "Select your programming language:",
-        options: [
-            { label: "TypeScript", value: "ts" },
-            { label: "JavaScript", value: "js" },
-        ],
-    })) as Config["language"];
+        options: languages.map(
+            ({ label, emoji, value }: INTERACTIVE_PROMPTS) => ({
+                label: `${label} ${emoji}`,
+                value: value,
+            }),
+        ),
+    });
 
     if (isCancel(language)) terminate("Process cancelled ❌");
 
-    return language;
+    return language as Config["language"];
 }
 
 async function promptDatabase(): Promise<Config["database"]> {
-    const db = (await select({
-        message: "Select your database:",
-        options: [
-            { label: "MySQL", value: "mysql" },
-            { label: "MariaDB", value: "mariadb" },
-            { label: "Sqlite", value: "sqlite" },
-            { label: "PostgreSQL", value: "postgres" },
-            { label: "MongoDB", value: "mongodb" },
-        ],
-    })) as Config["database"];
+    const database = await select({
+        message: "Choose your database",
+        options: databases.map(
+            ({ label, emoji, value }: INTERACTIVE_PROMPTS) => ({
+                label: `${label} ${emoji}`,
+                value: value,
+            }),
+        ),
+    });
 
-    if (isCancel(db)) terminate("Process cancelled ❌");
+    if (isCancel(database)) terminate("Process cancelled ❌");
 
-    return db;
+    return database as Config["database"];
 }
 
 async function promptPkgManager(): Promise<Config["packageManager"]> {
-    const pkgManager = (await select({
+    const pkgManager = await select({
         message: "Select your package manager:",
-        options: [
-            { label: "npm", value: "npm" },
-            { label: "yarn", value: "yarn" },
-            { label: "pnpm", value: "pnpm" },
-            { label: "bun", value: "bun" },
-        ],
-    })) as Config["packageManager"];
+        options: pkg_managers.map(
+            ({ label, emoji, value }: INTERACTIVE_PROMPTS) => ({
+                label: `${label} ${emoji}`,
+                value: value,
+            }),
+        ),
+    });
 
     if (isCancel(pkgManager)) terminate("Process cancelled ❌");
 
-    return pkgManager;
+    return pkgManager as Config["packageManager"];
 }
 
 async function promptCache(): Promise<Config["cache"]> {
